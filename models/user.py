d8acd7a3e9e08923fbc91937d93ce9209bc83f15
Nouf-Ashry <noufikashry@gmail.com>
2023-12-17 14:32:25 +0000

    user.py

--- models/user.py
+++ models/user.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
-""" the User class"""
+"""This module defines a class User"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """class User.
-
-    Attributes:
-        email (str): empty string.
-        password (str): empty string.
-        first_name (str): empty string.
-        last_name (str): empty string.
-    """
-
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    """This class defines a user by various attributes"""
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
