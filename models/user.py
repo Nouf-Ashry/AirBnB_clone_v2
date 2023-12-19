7f5d40a9e1d34a5d5c6494c22bf284e4c8cc9266
Nouf-Ashry <noufikashry@gmail.com>
2023-12-19 08:02:37 +0000

    user.py

--- models/user.py
+++ models/user.py
@@ -18,4 +18,4 @@ class User(BaseModel, Base):
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True
+    last_name = Column(String(128), nullable=True)
