78ebed8dabc7fcc136fc8cef4e560a974cf052d0
Nouf Ashry <noufikashry@gmail.com>
2024-01-31 08:40:55 +0000

    Air

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-""" starts a Flask web application"""
+#!/usr/bin/python3
+""" Starts a Flash Web Application """
 from flask import Flask
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """print web"""
+    """ Prints a Message when / is called """
     return 'Hello HBNB!'
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
